e1f5c2f04362a2df3d1bd751c8e7a761d9e8f59b
Rezeda <dinievarezeda@mail.ru>
2022-12-08 20:58:49 +0300

    выполнены задания к обязательному дз #1

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-/* function numInMind() {
-    let num = 27;
-    return function displayNumInMind(){
-        return num;
-    };
-}
-
-let thinkOfNumber = numInMind();
-
-const isNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num) && num[0] !== ' ';
-};
-
-let game = function(num) {
-    const ask = +prompt("Угадай число от 1 до 100");
-    if (ask > num ) {
-        alert("Загаданное число меньше");
-        game(thinkOfNumber());
-    } else if (ask < num) {
-        alert("Загаданное число больше");
-        game(thinkOfNumber());
-    } else if (ask !== isNumber(num)){
-        alert("Введи число!");
-        game(thinkOfNumber());
-    } else if (ask === null) {
-        alert("Игра окончена");
-    } else if (ask === num) {
-        alert("Поздравляю, Вы угадали!!!");
-    }
-};
-
-game(thinkOfNumber()); */
-
 //Загаданное число должно храниться «в замыкании»
 function numInMind() {
     let num = 20;
@@ -64,7 +31,6 @@ function f1(num){
         alert("Введи число!");
         f1(num);
     }
-    
 }
 
 f1(displayNum());
